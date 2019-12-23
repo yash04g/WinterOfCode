b76910d11bc75a9b78d660cbdd5b8172041bbd34
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2019-12-24 05:14:36 +0530

    Add files via upload

--- DP/Knapsack.cpp
+++ DP/Knapsack.cpp
@@ -1,34 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-int knapsnack(int* val,int* wt,int W,int n){
-	int** dp = new int*[n+1];
-	for(int i=0;i<=n;i++){
-		dp[i] = new int[W+1];
-	}
-
-	for(int i=0;i<=W;i++){
-		dp[0][i] = 0;
-	}
-	for(int i=0;i<=n;i++){
-		dp[i][0] = 0;
-	}
-
-	for(int i=1;i<=n;i++){
-		for(int w=0;w<=W;w++){
-
-			dp[i][w] = dp[i-1][w];
-			if(wt[i-1] <= w){
-				dp[i][w] = max(dp[i][w],val[i-1] + dp[i-1][w-wt[i-1]]);
-			}
-		}
-	}
-
-	int ans = dp[n][W];
-	//delete dp array 
-	return ans;
+int knapsack(int* values, int* weights,int maxWeight,int n){
+    int dp[2][10005]={-1};
+    for(int i=0;i<=maxWeight;i++){
+        dp[0][i] = 0;
+    }
+    int flag = 1;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<=maxWeight;j++){
+            dp[flag][j] = dp[flag^1][j];
+            if(weights[i-1]<=j){
+                dp[flag][j] = max(dp[flag][j],values[i-1]+dp[flag^1][j-weights[i-1]]);
+            }
+        }
+        flag = flag^1;
+    }
+    return dp[flag^1][maxWeight];
 }
+
 int main(){
 	
 	int val[] = {5,4,8,6};
@@ -37,5 +27,5 @@ int main(){
 	int W = 5;
 	int n = 4;
 
-	cout << knapsnack(val,wt,W,n)<<endl;
+	cout << knapsack(val,wt,W,n)<<endl;
 }
