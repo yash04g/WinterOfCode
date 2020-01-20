a3436a62a1ce66029a9154053ce4a9aa603e92ee
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2020-01-20 17:58:20 +0530

    Add files via upload

---
+++ Virtual/#570Div3/C.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+
+#define int long long
+#define ff first
+#define ss second
+#define loop(i,k,n) for(int i=k;i<n;++i)
+#define rloop(i,n,k) for(int i=n;i>=k;i--)
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define vpii vector<pair<int,int> >
+#define all(v) v.begin(),v.end()
+#define mset(a,i) memset(a,i,sizeof(a))
+#define mii map<int,int>
+#define mci map<char,int>  
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define deb(x) cout<<#x<<" "<<x<<endl;
+#define mp make_pair
+#define pb push_back
+#define mod 1000000007  
+#define endl "\n"
+#define inf 1e18
+#define sp(x,y) fixed<<setprecision(y)<<x
+
+void get_it_done(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+int gcd(int a, int b){ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+void swap(int &a,int &b){
+	a^=b;b^=a;a^=b;
+}
+const int N = 2e5+5;
+int dp[N];
+
+
+int32_t main(){
+	get_it_done();
+    int t=1;
+    cin >> t;
+    while (t--){
+        int k,n,a,b;
+        cin>>k>>n>>a>>b;
+        if(b*n>=k){
+            cout<<-1<<endl;
+            continue;
+        }
+        int ans = n;
+        int x = k-b*n;
+        if(x%(a-b)){
+            ans = min(x/(a-b),ans);
+        }else{
+            ans = min(x/(a-b)-1,ans);
+        }
+        cout<<ans<<endl;
+    }
+}
---
+++ Virtual/#570Div3/D.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+
+#define int long long
+#define ff first
+#define ss second
+#define loop(i,k,n) for(int i=k;i<n;++i)
+#define rloop(i,n,k) for(int i=n;i>=k;i--)
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define vpii vector<pair<int,int> >
+#define all(v) v.begin(),v.end()
+#define mset(a,i) memset(a,i,sizeof(a))
+#define mii map<int,int>
+#define mci map<char,int>  
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define deb(x) cout<<#x<<" "<<x<<endl;
+#define mp make_pair
+#define pb push_back
+#define mod 1000000007  
+#define endl "\n"
+#define inf 1e18
+#define sp(x,y) fixed<<setprecision(y)<<x
+
+void get_it_done(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+int gcd(int a, int b){ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+void swap(int &a,int &b){
+	a^=b;b^=a;a^=b;
+}
+const int N = 2e5+5;
+int dp[N];
+
+
+int32_t main(){
+	get_it_done();
+    int t=1;
+    cin >> t;
+    while (t--){
+        int n;
+        cin>>n;
+        vi a(n);
+        loop(i,0,n) cin>>a[i];
+        map<int,int> m;
+        loop(i,0,n) m[a[i]]++;
+        vi freq;
+        for(auto x:m){
+            freq.pb(x.ss);
+        }
+        // for(auto x:freq) cout<<x<<endl;
+        sort(all(freq),greater<int>());
+        priority_queue<int,vector<int>,greater<int> > pq;
+        set<int> s;
+        loop(i,0,freq.size()){
+            if(pq.empty()){
+                s.insert(freq[i]);       
+                pq.push(freq[i]);
+            }else{
+                if(freq[i]<pq.top()){
+                    s.insert(freq[i]);
+                    pq.push(freq[i]);
+                }else{
+                    if(pq.top()<=1){
+                        break;
+                    }
+                    s.insert(pq.top()-1);
+                    pq.push((pq.top()-1));
+                }
+            }
+        }
+        int ans = 0;
+        for(auto x:s){
+            // cout<<x<<endl;
+            ans += x;
+        }
+        cout<<ans<<endl;
+    }
+}
