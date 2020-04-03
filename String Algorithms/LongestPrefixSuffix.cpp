2889405ed2d366c3938843f22a28eecb88b84869
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2020-04-03 06:38:46 +0530

    Add files via upload

---
+++ String Algorithms/LongestPrefixSuffix.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+
+#define int long long
+#define ff first
+#define ss second
+#define loop(i,k,n) for(int i=k;i<n;++i)
+#define rloop(i,n,k) for(int i=n;i>=k;i--)
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define vpii vector<pair<int,int> >
+#define all(v) v.begin(),v.end()
+#define mset(a,i) memset(a,i,sizeof(a))
+#define mii map<int,int>  
+#define pii pair<int,int>
+#define deb(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#define mp make_pair
+#define pb push_back
+#define mod 1000000007  
+#define endl "\n"
+#define inf 1e18
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { if(0) return; cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args){   
+    if(0) return; 
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+inline int add(int x, int y) { x += y; if (x >= mod) x -= mod; return x; }
+inline int sub(int x, int y) { x -= y; if (x < 0) x += mod; return x; }
+inline int mul(int x, int y) { return ((x % mod) * (y % mod)) % mod; }
+void get_it_done(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+const int N = 1e6+5;
+int lps[N];
+
+void getLPS(string s){
+    mset(lps,0);
+    int i=1,j=0;
+    lps[0] = 0;
+    int n = s.length();
+    while(i<n){
+        if(s[i]==s[j]){
+            lps[i] = j+1;
+            i++;
+            j++;
+        }else{
+            if(j!=0){
+                j = lps[j-1];
+            }else{
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+int32_t main(){
+    get_it_done();
+    int q=1;
+    // cin >> q;
+    while (q--){
+        string s;
+        cin>>s;
+        getLPS(s);
+        
+    }
+}
