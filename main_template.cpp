44dc8b18d4e0054f8b38a907faa655b965f4d957
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2020-06-05 06:57:15 +0530

    Add files via upload

--- main_template.cpp
+++ main_template.cpp
@@ -1,16 +1,20 @@
 #include <bits/stdc++.h>
 #include<unordered_map>
 #include<unordered_set>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
 
 #define int long long
+#define ordered_set tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update>
 #define ff first
 #define ss second
 #define loop(i,k,n) for(int i=k;i<n;++i)
 #define rloop(i,n,k) for(int i=n;i>=k;i--)
 #define vi vector<int>
-#define vvi vector<vector<int> >
-#define vpii vector<pair<int,int> >
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int,int>>
 #define all(v) v.begin(),v.end()
 #define mset(a,i) memset(a,i,sizeof(a))
 #define mii map<int,int>  
@@ -30,26 +34,27 @@ void __f (const char* names, Arg1&& arg1, Args&&... args){
     const char* comma = strchr (names + 1, ',');
     cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
 }
-inline int add(int x, int y) { x += y; if (x >= mod) x -= mod; return x; }
-inline int sub(int x, int y) { x -= y; if (x < 0) x += mod; return x; }
-inline int mul(int x, int y) { return ((x % mod) * (y % mod)) % mod; }
 void get_it_done(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 }
-int gcd(int a, int b){ 
-    if (a == 0) 
-        return b; 
-    return gcd(b % a, a); 
-} 
+int gcd(int a,int b){
+    if(b==0) return a;
+    if(a<b) return gcd(b,a);
+    return gcd(b,a%b);
+}
 const int N = 3e5+5;
+
+void solve(){
+
+}
 
 int32_t main(){
     get_it_done();
-    int t=1;
-    cin >> t;
-    while (t--){
-        
+    int tc=1;
+    cin >> tc;
+    while (tc--){
+        solve();        
     }
 }
