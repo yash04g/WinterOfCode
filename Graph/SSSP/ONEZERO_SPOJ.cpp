dcbd8e8a3c503a3f555027f6ffb23a7f2677643d
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2020-07-22 20:07:13 +0530

    Add files via upload

---
+++ Graph/SSSP/ONEZERO_SPOJ.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define int long long int
+#define ff first
+#define ss second
+#define loop(i,k,n) for(int i=k;i<n;++i)
+#define rloop(i,n,k) for(int i=n;i>=k;i--)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int,int>>
+#define all(v) v.begin(),v.end()
+#define mset(a,i) memset(a,i,sizeof(a))
+#define mii map<int,int>  
+#define pii pair<int,int>
+#define deb(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#define mp make_pair
+#define pb push_back
+#define mod 1000000007  
+#define endl "\n"
+#define inf 1e18
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef tree<pii, null_type, less<pii>, rb_tree_tag, tree_order_statistics_node_update> pbds1;
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { if(0) return; cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args){   
+    if(0) return; 
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+void get_it_done(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+int gcd(int a,int b){
+    if(b==0) return a;
+    if(a<b) return gcd(b,a);
+    return gcd(b,a%b);
+}
+const int N = 2e4+5;
+int n;
+
+string solve(){
+    cin>>n; 
+    if(n==1){
+        return "1";
+    }
+    vector<string> dp(n+1);
+    dp[1] = "1";
+    queue<int> q;
+    q.push(1);
+    while(!q.empty()){
+        int r = q.front();
+        q.pop();
+        if(r==0){
+            return dp[r];
+        }
+        for(int digit:{0,1}){
+            int new_r = (r*10+digit)%n;
+            if(dp[new_r].empty()){
+                q.push(new_r);
+                dp[new_r] = dp[r]+char(digit+'0');
+            }
+        }
+    }
+    return "";
+}
+
+int32_t main(){
+    get_it_done(); 
+    int tc=1;
+    cin >> tc;
+    while (tc--){
+        string ans = solve();        
+        cout<<ans<<endl;
+    }
+}
